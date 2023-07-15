c9311371e89f06f39dd9c27b91523ff7b54138ec
CrabVS <139277442+CrabVS@users.noreply.github.com>
2023-07-15 15:54:57 +0200

    Add operators and calculator funcs

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('works');
+const add = function addNumbers(numOne, numTwo) {
+    return numOne + numTwo;
+}
+
+const subtract = function subtractNumbers(numOne, numTwo) {
+    return numOne - numTwo;
+}
+
+const multiply = function multiplyNumbers(numOne, numTwo) {
+    return numOne * numTwo;
+}
+
+const divide = function addNumbers(numOne, numTwo) {
+    return numOne / numTwo;
+}
+
+const calculator = function calculatorController() {
+    let firstNum = null;
+    let secondNum = null;
+    let operator = null;
+}
