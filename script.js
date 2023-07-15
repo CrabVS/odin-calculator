df6f7dd87f07bd883815baca399822906975a4c8
CrabVS <139277442+CrabVS@users.noreply.github.com>
2023-07-15 16:53:44 +0200

    Add calculator buttons

--- script.js
+++ script.js
@@ -34,8 +34,40 @@ const operate = function operate(numOne, numTwo, operator) {
     }
 }
 
+const createCalculator = function createCalculatorButtons() {
+    const calcBtnsEl = document.getElementById('calculator-buttons');
+    const buttonSymbols = [
+        ['←', 'ce', 'c', '/'],
+        ['7', '8', '9', '*'],
+        ['4', '5', '6', '-'],
+        ['1', '2', '3', '+'],
+        ['±', '0', '.', '=']
+    ]
+
+    let buttons = [];
+
+    buttonSymbols.forEach(row => {
+        const buttonsRowEl = document.createElement('div');
+        buttonsRowEl.classList.add('btn-row');
+        row.forEach(symbol => {
+            const buttonEl = document.createElement('button');
+            buttonEl.textContent = symbol;
+            buttonEl.classList.add('btn');
+            buttonsRowEl.appendChild(buttonEl);
+            buttons.push(buttonEl);
+        })
+
+        calcBtnsEl.appendChild(buttonsRowEl);
+    });
+}
+
+
 const calculator = function calculatorController() {
     let firstNum = null;
     let secondNum = null;
     let operator = null;
+
+    createCalculator();
 }
+
+calculator();
